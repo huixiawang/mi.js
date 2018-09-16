946e84dea84ce8431bf68c2c6e824707199fc9e9
huixiawang <2234615709@qq.com>
2018-09-16 23:15:49 +0800

    js

--- for-you.js
+++ for-you.js
@@ -1,46 +1,46 @@
-window.onload=function(){
+window.onload = function () {
     // 为你推荐
-    let button1=document.querySelector(".for-you .box1");
-    let button2=document.querySelector(".for-you .box2");
+    let button1 = document.querySelector(".for-you .box1");
+    let button2 = document.querySelector(".for-you .box2");
     // let container=document.querySelector(".for-you .container");
-    let ul=document.querySelector(".for-you ul");
-    let w=parseInt(getComputedStyle(ul,null).width)/3;
-    let times=0;
+    let ul = document.querySelector(".for-you ul");
+    let w = parseInt(getComputedStyle(ul, null).width) / 3;
+    let times = 0;
     console.log(ul);
-    button1.onclick=function(){
+    button1.onclick = function () {
         times++;
-        if(times==3){
-            times=2;
+        if (times == 3) {
+            times = 2;
         }
-        ul.style.transform=`translate(${(-w*times)}px)`;
+        ul.style.transform = `translate(${(-w * times)}px)`;
     }
-    button2.onclick=function(){
+    button2.onclick = function () {
         times--;
-        if(times==-1){
-            times=0;
+        if (times == -1) {
+            times = 0;
         }
-        ul.style.transform=`translate(${(-w*times)}px)`;
+        ul.style.transform = `translate(${(-w * times)}px)`;
     }
 
     // 开始闪购
-    let button=document.querySelectorAll(".xm-flashover .box-hd .con");
+    let button = document.querySelectorAll(".xm-flashover .box-hd .con");
     // let container=document.querySelector(".for-you .container");
-    let ull=document.querySelector(".xm-flashover .box-bd ul");
-    let ws=parseInt(getComputedStyle(ull,null).width)/3;
-    let time=0;
-    button[0].onclick=function(){
+    let ull = document.querySelector(".xm-flashover .box-bd ul");
+    let ws = parseInt(getComputedStyle(ull, null).width) / 3;
+    let time = 0;
+    button[0].onclick = function () {
         time++;
-        if(time==2){
-            time=1;
+        if (time == 2) {
+            time = 1;
         }
-        ull.style.transform=`translate(${(-ws*time)}px)`;
+        ull.style.transform = `translate(${(-ws * time)}px)`;
     }
-    button[1].onclick=function(){
+    button[1].onclick = function () {
         time--;
-        if(time==-1){
-            time=0;
+        if (time == -1) {
+            time = 0;
         }
-        ull.style.transform=`translate(${(-ws*time)}px)`;
+        ull.style.transform = `translate(${(-ws * time)}px)`;
     }
 
     // 顶部banner轮播
@@ -118,54 +118,59 @@ window.onload=function(){
     }
 
     //////////////////////////////////////////////
-    let lis=document.querySelector(".content ul li");
-    let hook=document.querySelectorAll(".content .booh .first");
-    let dot1=document.querySelectorAll(".content ul li .pages .ctrile");
-    let leftb=document.querySelector(".content ul li .leftb");
-    let rightb=document.querySelector(".content ul li .rightb");
-    let widtha=parseInt(getComputedStyle(lis,null).width);
-    console.log(hook,dot1,leftb,rightb,widtha);
-    function frist(hook,dot1,leftb,rightb,widtha){
-        hook[0].style.left=0;
+    // 内容轮播图
+    let lis = document.querySelector(".content ul li");
+    let hook = document.querySelectorAll(".content .booh .first");
+    let dot1 = document.querySelectorAll(".content ul li .pages .ctrile.aa");
+    let leftb = document.querySelector(".content ul li .leftb.aa");
+    let rightb = document.querySelector(".content ul li .rightb.aa");
+    let widtha = parseInt(getComputedStyle(lis, null).width);
+    console.log(hook, dot1, leftb, rightb, widtha);
+
+    function frist(hook, dot1, leftb, rightb, widtha) {
+        hook[0].style.left = 0;
         dot1[0].classList.add("active");
-        let p_flag=true;
-        let current=down=0;
+        let p_flag = true;
+        let current = down = 0;
+
         // let tt=setInterval(moveLeft,2000);
         function moveR() {
             down++;
-            if(down==hook.length){
-                down=0;
+            if (down == hook.length) {
+                down = 0;
             }
-            hook[down].style.left=`${widtha}px`;
-            animate(hook[current],{left:-widtha});
-            animate(hook[down],{left:0},function () {
-                p_flag=true;
+            hook[down].style.left = `${widtha}px`;
+            animate(hook[current], {left: -widtha});
+            animate(hook[down], {left: 0}, function () {
+                p_flag = true;
             });
             dot1[current].classList.remove("active");
             dot1[down].classList.add("active");
-            current=down;
+            current = down;
         }
+
         function moveLeft() {
             down--;
-            if(down==-1){
-                down=hook.length-1;
+            if (down == -1) {
+                down = hook.length - 1;
             }
-            hook[down].style.left=`-${widtha}px`;
-            animate(hook[current],{left:widtha});
-            animate(hook[down],{left:0},function () {
-                p_flag=true;
+            hook[down].style.left = `-${widtha}px`;
+            animate(hook[current], {left: widtha});
+            animate(hook[down], {left: 0}, function () {
+                p_flag = true;
             });
-            dot1[current].classList.remove("activd");
-            dot1[down].classList.add("activd");
-            current=down;
+            dot1[current].classList.remove("active");
+            dot1[down].classList.add("active");
+            current = down;
         }
-        leftb.onclick=function () {
-            if(p_flag==false){
+
+        leftb.onclick = function () {
+            if (p_flag == false) {
                 return;
             }
-            p_flag=false;
-            if(down==0){
-                p_flag=true;
+            p_flag = false;
+            if (down == 0) {
+                p_flag = true;
                 return;
             }
             moveLeft();
@@ -181,50 +186,92 @@ window.onload=function(){
             }
             moveR();
         };
-        for(let i=0;i<dot1.length;i++){
-            dot1[i].onclick=function () {
-                for(let j=0;j<dot1.length;j++){
+        for (let i = 0; i < dot1.length; i++) {
+            dot1[i].onclick = function () {
+                for (let j = 0; j < dot1.length; j++) {
                     dot1[j].classList.remove("active");
-                    hook[j].style.left=`${widtha}px`;
+                    hook[j].style.left = `${widtha}px`;
                 }
                 dot1[i].classList.add("active");
-                hook[i].style.left=0;
-                current=down=i;
+                hook[i].style.left = 0;
+                current = down = i;
             }
         }
     }
-    frist(hook,dot1,leftb,rightb,widtha);
-    let hook1=document.querySelectorAll(".content .hoop .first1");
-    let dot2=document.querySelectorAll(".content ul li .pages.a .ctrile.a");
-    let leftb1=document.querySelector(".content ul li .leftb.a");
-    let rightb1=document.querySelector(".content ul li .rightb.a");
-    frist(hook1,dot2,leftb1,rightb1,widtha);
-    let hook2=document.querySelectorAll(".content .hook1 .first2");
-    let dot3=document.querySelectorAll(".content ul li .ctrile.x");
-    let leftb2=document.querySelector(".content ul li .leftb.x");
-    let rightb2=document.querySelector(".content ul li .rightb.x");
-    frist(hook2,dot3,leftb2,rightb2,widtha);
-    let hook3=document.querySelectorAll(".content .hoop1 .first3");
-    let dot4=document.querySelectorAll(".content ul li .ctrile.c");
-    let leftb3=document.querySelector(".content ul li .leftb.c");
-    let rightb3=document.querySelector(".content ul li .rightb.c");
-    frist(hook3,dot4,leftb3,rightb3,widtha);
 
-
+    frist(hook, dot1, leftb, rightb, widtha);
+    let hook1 = document.querySelectorAll(".content .hoop .first1");
+    let dot2 = document.querySelectorAll(".content ul li .pages.a .ctrile.a");
+    let leftb1 = document.querySelector(".content ul li .leftb.a");
+    let rightb1 = document.querySelector(".content ul li .rightb.a");
+    frist(hook1, dot2, leftb1, rightb1, widtha);
+    let hook2 = document.querySelectorAll(".content .hook1 .first2");
+    let dot3 = document.querySelectorAll(".content ul li .ctrile.x");
+    let leftb2 = document.querySelector(".content ul li .leftb.x");
+    let rightb2 = document.querySelector(".content ul li .rightb.x");
+    frist(hook2, dot3, leftb2, rightb2, widtha);
+    let hook3 = document.querySelectorAll(".content .hoop1 .first3");
+    let dot4 = document.querySelectorAll(".content ul li .ctrile.c");
+    let leftb3 = document.querySelector(".content ul li .leftb.c");
+    let rightb3 = document.querySelector(".content ul li .rightb.c");
+    frist(hook3, dot4, leftb3, rightb3, widtha);
 
 
     // 家电选项卡
-    let num=document.querySelectorAll(".tv .title ul li");
-    let son=document.querySelectorAll(".tv ul .right");
-    console.log(num,son);
-    for(let i=0;i<num.length;i++){
-        num[i].onmouseover=function () {
-            for(let j=0;j<num.length;j++){
-                son[j].style.display="none";
+    let num = document.querySelectorAll(".tv .title ul li");
+    let son = document.querySelectorAll(".tv ul .right");
+    console.log(num, son);
+    for (let i = 0; i < num.length; i++) {
+        num[i].onmouseover = function () {
+            for (let j = 0; j < num.length; j++) {
+                son[j].style.display = "none";
                 num[j].classList.remove("active");
             }
-            son[i].style.display="block";
+            son[i].style.display = "block";
             num[i].classList.add("active");
         }
     }
+
+    // 导航选项卡
+    let tt = document.querySelectorAll(".top-nav ul li a");
+    let white = document.querySelectorAll(".top-nav .white-box");
+    console.log(tt);
+    for (let i = 0; i < tt.length; i++) {
+        tt[i].onmouseenter = function () {
+            for (let j = 0; j < tt.length; j++) {
+                white[j].style.height = `${230}px`;
+                white[j].style.zIndex = 1;
+                tt[j].classList.remove("active");
+                white[j].style.borderTop = "1px solid #e0e0e0";
+            }
+            white[i].style.height = `${230}px`;
+            white[i].style.borderTop = "1px solid #e0e0e0";
+            tt[i].classList.add("active");
+            white[i].style.zIndex = 999;
+        }
+        tt[i].onmouseleave = function () {
+            for (let i = 0; i < tt.length; i++) {
+                white[i].style.height = 0;
+                tt[i].classList.remove("active");
+                white[i].style.borderTop = "none";
+            }
+        }
+    }
+
+    // 返回顶部
+    let backs = document.querySelector(".bartop ul li.back");
+    console.log(backs);
+    backs.style.display = "none";
+    backs.onclick = function () {
+        animate(document.body, {scrollTop: 0});
+        animate(document.documentElement, {scrollTop: 0});
+    }
+    document.onscroll = function () {
+        if (document.body.scrollTop || document.documentElement.scrollTop >= 1000) {
+            backs.style.display = "block";
+        } else {
+            backs.style.display = "none";
+        }
+    }
+
 }
